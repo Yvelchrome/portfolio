4b8c5eef284106bd2494da9a8fb5e914dc4ef2ac
Yvelchrome <yvelchrome@gmail.com>
2022-08-30 19:05:55 +0200

    feat: add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roobert400: ["roobert-regular", "sans-serif"],
+        roobert500: ["roobert-medium", "sans-serif"],
+        roobert600: ["roobert-semibold", "sans-serif"],
+        roxborough400: ["roxborough-regular", "sans-serif"],
+        roxborough600: ["roxborough-semibold", "sans-serif"],
+        roxborough600i: ["roxborough-semibolditalic", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
