a40b5c026c0f1205a4d8d54fc73da437ad3d251e
Yvelchrome <yvelchrome@gmail.com>
2022-09-07 21:39:54 +0200

    feat: custom col max-content class
    Creation of a new class and renaming others

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,12 +4,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roobert400: ["roobert-regular", "sans-serif"],
-        roobert500: ["roobert-medium", "sans-serif"],
-        roobert600: ["roobert-semibold", "sans-serif"],
-        roxborough400: ["roxborough-regular", "sans-serif"],
-        roxborough600: ["roxborough-semibold", "sans-serif"],
-        roxborough600i: ["roxborough-semibolditalic", "sans-serif"],
+        "roobert-400": ["roobert-regular", "sans-serif"],
+        "roobert-500": ["roobert-medium", "sans-serif"],
+        "roobert-600": ["roobert-semibold", "sans-serif"],
+        "roxborough-400": ["roxborough-regular", "sans-serif"],
+        "roxborough-600": ["roxborough-semibold", "sans-serif"],
+        "roxborough-600i": ["roxborough-semibolditalic", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "2-max": "repeat(2, max-content)",
       },
     },
   },
