4c90c0007571b228876b7c744e8e02a953031292
Yvelchrome <Yvelchrome@gmail.com>
2023-07-05 16:24:43 +0200

    refactor: nextjs font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,59 @@
-import "../styles/globals.css";
+import "./app.css";
+import localFont from "next/font/local";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { Footer, Header } from "components";
+
+const Roobert = localFont({
+  src: [
+    {
+      path: "../assets/fonts/Roobert_Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Roobert_Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/Roobert_SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-roobert",
+});
+const Roxborough = localFont({
+  src: [
+    {
+      path: "../assets/fonts/RoxboroughCF_Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/RoxboroughCF_SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/RoxboroughCF_SemiBoldItalic.otf",
+      weight: "600",
+      style: "italic",
+    },
+  ],
+  variable: "--font-roxborough",
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <main className={`${Roobert.variable} ${Roxborough.variable} bg-white`}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default MyApp;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,12 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "roobert-400": ["roobert-regular", "sans-serif"],
-        "roobert-500": ["roobert-medium", "sans-serif"],
-        "roobert-600": ["roobert-semibold", "sans-serif"],
-        "roxborough-400": ["roxborough-regular", "sans-serif"],
-        "roxborough-600": ["roxborough-semibold", "sans-serif"],
-        "roxborough-600i": ["roxborough-semibolditalic", "sans-serif"],
+        roobert: ["var(--font-roobert)", "sans-serif"],
+        roxborough: ["var(--font-roxborough)", "sans-serif"],
       },
       gridTemplateColumns: {
         "2-max": "repeat(2, max-content)",
