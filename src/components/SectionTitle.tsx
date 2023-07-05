bede815f5a61a130d77902b40d9ee7f95e9b3e40
Yvelchrome <Yvelchrome@gmail.com>
2023-07-05 16:26:04 +0200

    feat: tailwind custom colors

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,10 +1,16 @@
-function SectionTitle({ number, title }: { number: string; title: string }) {
+export default function SectionTitle({
+  number,
+  title,
+}: {
+  number: string;
+  title: string;
+}) {
   return (
     <div className="my-12">
-      <h3 className="font-roobert-600 text-sm text-gray-500">{number}</h3>
-      <h2 className="font-roobert-500 text-base text-black">{title}</h2>
+      <p className="font-roobert text-sm font-semibold text-light-grey">
+        {number}
+      </p>
+      <h2 className="font-roobert text-base font-medium text-black">{title}</h2>
     </div>
   );
 }
-
-export default SectionTitle;
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -3,7 +3,7 @@ import moon from "../assets/images/theme_moon.svg";
 
 function ThemeSwitch() {
   return (
-    <button className="fixed right-12 bottom-12 flex h-10 w-10 items-center justify-center bg-gray-300">
+    <button className="fixed right-12 bottom-12 flex h-10 w-10 items-center justify-center bg-theme-switch">
       <Image src={moon} alt="" height={20} width={20} />
     </button>
   );
