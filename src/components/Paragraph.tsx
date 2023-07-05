e6f821446aac3bfd2b9b337efa7d409da6f9e6f2
Yvelchrome <Yvelchrome@gmail.com>
2023-07-05 16:30:09 +0200

    feat: Paragraph.tsx (Section 1: About me)

---
+++ src/components/Paragraph.tsx
@@ -0,0 +1,40 @@
+export default function SectionTitle() {
+  return (
+    <div className="my-12">
+      <p className="font-roobert text-[39px] font-normal text-black">
+        Hi, I'm Steven
+      </p>
+      <p className="font-roobert text-[39px] font-normal text-black">
+        I’m a French{" "}
+        <span className="font-roxborough font-normal text-dark-blue">
+          front-end developer
+        </span>{" "}
+        with a passion for building beautiful and functional websites with a
+        good user experience. Currently working{" "}
+        <a
+          className="text-dark-blue"
+          href="https://www.subskill.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @<span className="font-roxborough font-normal">Subskill</span>
+        </a>{" "}
+        and a bachelor student in web development{" "}
+        <a
+          className="text-dark-blue"
+          href="https://www.hetic.net"
+          target="_blank"
+          rel="noreferrer"
+        >
+          @<span className="font-roxborough font-normal">HETIC</span>
+        </a>
+        .
+      </p>
+      <p className="font-roobert text-[39px] font-normal text-black">
+        Even if I prefer the front-end for the design aspect, I don't intend to
+        limit myself to it and I aim to become a full-stack developer to reach
+        my potential.
+      </p>
+    </div>
+  );
+}
