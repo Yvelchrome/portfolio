3332be8004587a2e5603d21674e10c6adbcadb4a
Yvelchrome <yvelchrome@gmail.com>
2023-09-09 20:13:36 +0200

    refactor: codemods | next-image-to-legacy-image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import logo from 'images/logo.svg';
 import Link from 'next/link';
 
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/legacy/image";
 import moon from 'images/theme_moon.svg';
 
 export default function ThemeSwitch() {
