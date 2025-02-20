fcf73dfda1989e5491ca44082067bad5b807cba2
Yvelchrome <yvelchrome@gmail.com>
2025-02-20 19:44:45 +0100

    refactor: rm color replacement from tailwind config
    Colors are now extended

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,17 +6,6 @@ import * as tailwindAnimate from "tailwindcss-animate";
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    colors: {
-      black: "#000000",
-      "lighter-black": "#1C1C1C",
-      grey: "#333333",
-      "light-grey": "#4B4B4B",
-      white: "#FFFFFF",
-      "dark-blue": "#3399CC",
-      "light-blue": "#66CCFF",
-      "work-page-black": "#1E1E1E",
-      "theme-switch": "#D9D9D9",
-    },
     extend: {
       fontFamily: {
         satoshi: ["var(--font-satoshi)", ...fontFamily.sans],
