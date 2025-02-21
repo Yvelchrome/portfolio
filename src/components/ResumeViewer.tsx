80fdb7511324dd4e698afb997c0ddd34025c6921
Yvelchrome <yvelchrome@gmail.com>
2025-02-21 16:52:08 +0100

    feat: resume DialogDescription for accessibility

--- src/components/ResumeViewer.tsx
+++ src/components/ResumeViewer.tsx
@@ -2,6 +2,7 @@ import { useLocale, useTranslations } from "next-intl";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -24,6 +25,9 @@ const ResumeViewer = () => {
       <DialogContent className="flex h-5/6 max-w-screen-lg flex-col border-0">
         <DialogHeader>
           <DialogTitle className="font-bold">{t("resume")}</DialogTitle>
+          <DialogDescription className="sr-only">
+            Find my resume here : <a href={resumePath}>View Resume</a>
+          </DialogDescription>
         </DialogHeader>
         <iframe className="h-full w-auto" src={resumePath} />
       </DialogContent>
