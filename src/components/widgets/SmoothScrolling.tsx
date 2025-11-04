adc1dae88b5c96ef3ae234d7029db10d64a89e81
Yvelchrome <yvelchrome@gmail.com>
2025-11-05 00:11:53 +0100

    fix: follow-up to 801b93d

--- src/components/widgets/SmoothScrolling.tsx
+++ src/components/widgets/SmoothScrolling.tsx
@@ -1,22 +1,39 @@
 "use client";
 
 import { useEffect, useRef, type ReactNode } from "react";
-
 import { ReactLenis, type LenisRef } from "lenis/react";
 import { cancelFrame, frame } from "motion";
+import { usePathname } from "next/navigation";
+
+let globalLenis: LenisRef["lenis"] | null = null;
+export function getLenis() {
+  return globalLenis;
+}
 
 function SmoothScrolling({ children }: { children: ReactNode }) {
   const lenisRef = useRef<LenisRef>(null);
+  const pathname = usePathname();
 
   useEffect(() => {
+    const assignLenis = () => {
+      if (lenisRef.current?.lenis) {
+        globalLenis = lenisRef.current.lenis;
+      } else {
+        requestAnimationFrame(assignLenis);
+      }
+    };
+    assignLenis();
+
     function update(data: { timestamp: number }) {
-      const time = data.timestamp;
-      lenisRef.current?.lenis?.raf(time);
+      lenisRef.current?.lenis?.raf(data.timestamp);
     }
 
     frame.update(update, true);
 
-    return () => cancelFrame(update);
+    return () => {
+      cancelFrame(update);
+      globalLenis = null;
+    };
   }, []);
 
   return (
@@ -24,6 +41,7 @@ function SmoothScrolling({ children }: { children: ReactNode }) {
       root
       options={{ autoRaf: false, lerp: 0.1, duration: 1.5 }}
       ref={lenisRef}
+      key={pathname}
     >
       {children}
     </ReactLenis>
