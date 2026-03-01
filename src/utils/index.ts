4c8c77677c60e217fbe31b1cae1a54d82c934544
Yvelchrome <yvelchrome@gmail.com>
2026-03-01 15:28:27 +0100

    refactor(utils): update exports in index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,13 @@
 export { ConditionalWrapper } from "./ConditionalWrapper";
+export { getContactInfo } from "./GetContactInfo";
 export { getHeaderHeight } from "./GetHeaderHeight";
 export { LenisControl } from "./LenisControl";
 export {
+  BREAKPOINTS,
   matchesWidth,
   matchesHeight,
   getCurrentWidthBreakpoint,
   getCurrentHeightBreakpoint,
   useBreakpoint,
 } from "./WindowBreakpointDetection";
+export { hasWindow } from "./windowEnv";
