b500859893be0e96f85c663999b6d7c8837b9abc
Yvelchrome <yvelchrome@gmail.com>
2026-03-08 18:59:44 +0100

    chore: docs in globals.css for reference about var(--removed-body-scroll-bar-size)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
       initial="hidden"
       animate="visible"
       variants={fadeInFromTop}
-      className="bg-background fixed top-0 right-0 left-0 z-50 mr-[var(--removed-body-scroll-bar-size)] border-b py-4"
+      className="bg-background fixed top-0 right-0 left-0 z-50 border-b py-4 mr-(--removed-body-scroll-bar-size)"
     >
       <div className="container mx-auto flex items-center justify-between px-4 sm:px-8">
         <Logo />
